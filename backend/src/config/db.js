5b937d616ac9532e912313b0b159f9ca6ad90c8d
Mahilan Suki <mahilan.suki@gmail.com>
2026-01-02 13:14:02 +0530

    Connect to MongoDB
    - Use .env file and dotenv module to add private information
    - Add src/config/db.js to configure the database
    - Add connectDB() to connect to the database
    - Update server.js

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected successfully");
+    }
+    catch (error) {
+        console.error(`Error connecting to MongoDB: ${error}`);
+        process.exit(1);  // exit with failure
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 5001;
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(port, () => {
-    console.log(`Server running on PORT:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on PORT:${PORT}`);
 });
