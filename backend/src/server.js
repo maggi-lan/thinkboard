83d29d0aa55e4ae6e826f7b9735e91a9774c3605
Mahilan Suki <mahilan.suki@gmail.com>
2026-01-06 00:00:03 +0530

    Resolve CORS Error and Create Home Page
    - Add cors middleware before rateLimiter and router in server.js
    - Add components:
    	- Navbar
    	- Notecard
    	- RateLimitedUI
    - Add utils.js to use helper functions like formatDate()
    - Update HomePage.jsx

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 // Load .env file
@@ -14,6 +16,9 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());             // enable JSON body parsing
+app.use(cors({                       // allows frontend to access this API
+    origin: "http://localhost:5173",
+}));
 app.use(rateLimiter);                // adds a rate limiter
 app.use("/api/notes", notesRoutes);  // mounts the notes API routes
 
