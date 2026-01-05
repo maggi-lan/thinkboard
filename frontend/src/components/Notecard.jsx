b748e340a868e13e8a597c7e772cec278d033aa1
Mahilan Suki <mahilan.suki@gmail.com>
2026-01-06 01:27:51 +0530

    Add Delete Note Feature
    - Display NoNotesAvailable when the homepage is empty
    - Automatically re-render the homepage when a note is deleted

--- frontend/src/components/Notecard.jsx
+++ frontend/src/components/Notecard.jsx
@@ -1,9 +1,27 @@
 
 import { Link } from 'react-router';
+import { toast } from 'react-hot-toast';
 import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import { formatDate } from '../lib/utils.js';
+import api from '../lib/axios.js';
 
-function Notecard({note}) {
+function Notecard({ note, setNotes }) {
+    const handleDelete = async (e, id) => {
+        e.preventDefault();  // doesn't redirect to the card's detail page
+
+        if (!window.confirm("Are you sure that you want to delete this note?")) return;
+
+        try {
+            api.delete(`/notes/${id}`);
+            setNotes((prev) => prev.filter(note => note._id != id));  // gets rid of the deleted note and causes a re-render
+            toast.success("Note deleted successfully");
+        }
+        catch (error) {
+            console.log(`Error deleting the note: ${error}`);
+            toast.error("Failed to delete the note");
+        }
+    }
+
     return (
         <Link to={ `/note/${note._id}` } className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]">
             <div className="card-body">
@@ -15,7 +33,7 @@ function Notecard({note}) {
                     </span>
                     <div className="flex items-center gap-1">
                         <PenSquareIcon className="size-4" />
-                        <button className="btn btn-ghost btn-xs text-error">
+                        <button className="btn btn-ghost btn-xs text-error" onClick={ (e) => handleDelete(e, note._id) }>
                             <Trash2Icon className="size-4" />
                         </button>
                     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import toast from 'react-hot-toast';
 
 import Navbar from '../components/Navbar.jsx';
 import Notecard from '../components/Notecard.jsx';
+import NoNotesAvailable from '../components/NoNotesAvailable.jsx';
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 
 function HomePage() {
@@ -48,11 +49,13 @@ function HomePage() {
             <div className="max-w-7xl mx-auto p-4 mt-6">
                 { loading && <div className="text-center text-primary py-10">Loading notes...</div> }
 
+                { notes.length == 0 && !isRateLimited && !loading && <NoNotesAvailable /> }
+
                 { notes.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         { notes.map((note) => {
                             return (
-                                <Notecard key={ note._id } note={ note } />
+                                <Notecard key={ note._id } note={ note } setNotes={ setNotes } />
                             );
                         })}
                     </div>
